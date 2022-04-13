39d26b9acca11022252d9da779611603596cbc0b
Claire Thompson <clairethompson@Claires-MacBook-Pro.local>
2022-04-13 09:55:20 -0400

    added profile details

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -2,11 +2,21 @@ import * as tokenService from '../services/tokenService'
 
 const BASE_URL = `${process.env.REACT_APP_API_URL}/api/profiles`
 
-async function getAllProfiles() {
-  const res = await fetch(BASE_URL, {
-    headers: { Authorization: `Bearer ${tokenService.getToken()}` },
-  })
-  return await res.json()
+export const getAllProfiles = async () => {
+  try {
+    const res = await fetch(`${BASE_URL}`)
+    return await res.json()
+  } catch (error) {
+    throw error
+  }
 }
 
-export { getAllProfiles }
+export const getOne = async (id) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${id}`)
+    return await res.json()
+  } catch (error) {
+    throw error
+  }
+}
+// export { getAllProfiles }
