0af121f7644618350d20ea16eb291dd2568e2fb1
tyebean <flower.webster@gmail.com>
2022-04-13 20:16:16 -0400

    star message service

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import { Routes, Route, useNavigate, Navigate } from 'react-router-dom'
+
+// components
 import NavBar from './components/NavBar/NavBar'
+
+// pages
 import Signup from './pages/Signup/Signup'
 import Login from './pages/Login/Login'
 import Landing from './pages/Landing/Landing'
 import Profiles from './pages/Profiles/Profiles'
 import ProfileDetails from './pages/ProfileDetails/ProfileDetails'
+
+// services
 import * as authService from './services/authService'
 import * as profileService from './services/authService'
+// import * as messageService from '../services/messageService'
 
 const App = () => {
   const [user, setUser] = useState(authService.getUser())
@@ -23,6 +30,14 @@ const App = () => {
   const handleSignupOrLogin = () => {
     setUser(authService.getUser())
   }
+
+  // const [messages, setMessages] = useState([])
+
+  // const addMessage = async (messageData) => {
+  //   const message = await messageService.create(messageData)
+  //   setMessages([...message, message])
+  // }
+  // console.log("message is", messages)
 
   return (
     <>
